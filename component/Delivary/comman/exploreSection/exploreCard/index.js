3ccf6453e9d78ab20918622348acfad47fb889a3
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2022-03-11 17:18:02 +0530

    df

--- component/Delivary/comman/exploreSection/exploreCard/index.js
+++ component/Delivary/comman/exploreSection/exploreCard/index.js
@@ -31,6 +31,45 @@ const ExploreCard = ({ restaurent }) => {
         {goldOff && <div className="gold-off abolute-center">{goldOff}</div>}
         {discount && <div className="discount abolute-center">{discount}</div>}
       </div>
+      <div className="res-row">
+        <div className="res-name">{name}</div>
+        {rating && (
+          <div className="res-rating  absolute-center">
+            {rating}
+            <i className="fa-regular fa-star absolute-center"></i>
+          </div>
+        )}
+      </div>
+      <div className="res-row">
+        {cuisines.length && (
+          <div className="res-cuisine">
+            {cuisines.map((item, i) => {
+              return (
+                <span className="res-cuisine-tag">
+                  {item}
+                  {i !== cuisines.length - 1 && ","}{" "}
+                </span>
+              );
+            })}
+          </div>
+        )}
+        {approxPrice && <div className="res-price">{approxPrice}</div>}
+      </div>
+      {
+        (bottomContainer.length = 0 && (
+          <div>
+            <div className="card-separetor"></div>
+            <div className="explore-bottom">
+              <img
+                src={bottomContainer[0]?.image.url}
+                style={{ height: "18px" }}
+                alt={bottomContainer[0]?.text}
+              />
+              <div className="res-bottom-text"> {bottomContainer[0]?.text}</div>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
